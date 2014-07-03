9e763cb9d8527611608a3e5007a78312ea1bcee3
Neil Marion dela Cruz <nmfdelacruz@gmail.com>
2014-07-03 14:18:03 +0800

    Wildcard search

--- app/controllers/stores_controller.rb
+++ app/controllers/stores_controller.rb
@@ -1,7 +1,7 @@
 class StoresController < ApplicationController
   def index
     @stores = if params[:store_name] && !params[:store_name].blank?
-       Store.where(database_name: params[:store_name]).order(:database_name).page(params[:page])
+       Store.where("database_name LIKE ?", "%#{params[:store_name]}%").order(:database_name).page(params[:page])
     else
        Store.page(params[:page]).order(:database_name)
     end
