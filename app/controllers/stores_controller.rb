63a3e13476bab97e7d4b10e335a35d0c73fac928
Neil Marion dela Cruz <nmfdelacruz@gmail.com>
2014-07-03 14:14:30 +0800

    search

--- app/controllers/stores_controller.rb
+++ app/controllers/stores_controller.rb
@@ -1,5 +1,9 @@
 class StoresController < ApplicationController
   def index
-    @stores = Store.page params[:page]
+    @stores = if params[:store_name] && !params[:store_name].blank?
+       Store.where(database_name: params[:store_name]).order(:database_name).page(params[:page])
+    else
+       Store.page(params[:page]).order(:database_name)
+    end
   end
 end
